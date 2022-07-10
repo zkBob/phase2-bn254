c955b4162f46afb00cf5a90b5e2c4996eec9762b
vms <michail.vms@gmail.com>
2022-07-10 11:23:29 +0300

    fix phase2 fawkes-crypto compatibility

--- phase2/src/bin/get_vk.rs
+++ phase2/src/bin/get_vk.rs
@@ -7,7 +7,7 @@ extern crate rand;
 use std::fs::File;
 
 use fawkes_crypto::backend::bellman_groth16::{engines::Bn256, verifier::VK};
-use fawkes_crypto_phase2::parameters::MPCParameters;
+use fawkes_crypto::backend::bellman_groth16::Parameters;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -20,10 +20,11 @@ fn main() {
 
     println!("Exporting {}...", params_filename);
 
-    let fp = File::open(params_filename).unwrap();
-    let mpc = MPCParameters::read(fp, false, true).unwrap();
+    let fp = std::path::Path::new(params_filename);
+    let mut raw_parameters = std::fs::read(fp).unwrap();
+    let mpc = Parameters::<Bn256>::read(&mut raw_parameters.as_slice(), true, true).unwrap();
 
-    let vk = mpc.get_params().vk.clone();
+    let vk = mpc.0.vk.clone();
     let vk: VK<Bn256> = VK::from_bellman(&vk);
     let vk_str = serde_json::to_string_pretty(&vk).unwrap();
     std::fs::write(vk_filename, &vk_str.into_bytes()).unwrap();
--- phase2/src/bin/new.rs
+++ phase2/src/bin/new.rs
@@ -3,6 +3,7 @@ extern crate fawkes_crypto;
 extern crate fawkes_crypto_phase2;
 extern crate libzeropool;
 extern crate rand;
+extern crate bellman_ce;
 
 use libzeropool::{
     circuit::tree::{tree_update, CTreePub, CTreeSec},
@@ -10,6 +11,7 @@ use libzeropool::{
     POOL_PARAMS,
 };
 use std::fs::File;
+use bellman_ce::pairing::CurveAffine;
 
 use fawkes_crypto::{
     backend::bellman_groth16::{
@@ -20,6 +22,46 @@ use fawkes_crypto::{
     core::signal::Signal,
 };
 use fawkes_crypto_phase2::parameters::MPCParameters;
+use fawkes_crypto::backend::bellman_groth16::Parameters;
+use fawkes_crypto::circuit::cs::CS;
+use fawkes_crypto::engines::bn256::Fr;
+
+fn tx_circuit<C:CS<Fr=Fr>>(public: CTransferPub<C>, secret: CTransferSec<C>) {
+    c_transfer(&public, &secret, &*POOL_PARAMS);
+}
+
+/*
+pub fn prepare_parameters<E: Engine, Pub: Signal<BuildCS<E::Fr>>, Sec: Signal<BuildCS<E::Fr>>, C: Fn(Pub, Sec)>(
+    circuit: C,
+) -> Parameters<E> {
+    let ref rcs = BuildCS::rc_new();
+    let signal_pub = Pub::alloc(rcs, None);
+    signal_pub.inputize();
+    let signal_sec = Sec::alloc(rcs, None);
+
+    circuit(signal_pub, signal_sec);
+
+    let bcs = BellmanCS::<E, BuildCS<E::Fr>>::new(rcs.clone());
+
+    let ref mut rng = OsRng::new();
+    let bp = bellman::groth16::generate_random_parameters(bcs, rng).unwrap();
+    let cs=rcs.borrow();
+
+    let num_gates = cs.gates.len();
+
+    let mut buf = std::io::Cursor::new(vec![]);
+    let mut c = brotli::CompressorWriter::new(&mut buf, 4096, 9, 22);
+    for g in cs.gates.iter() {
+        c.write_all(&g.try_to_vec().unwrap()).unwrap();
+    }
+
+    c.flush().unwrap();
+    drop(c);
+
+    Parameters(bp, num_gates as u32, buf.into_inner(), cs.const_tracker.clone())
+}
+
+ */
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -46,21 +88,26 @@ fn main() {
             signal_pub.inputize();
             let signal_sec = CTransferSec::alloc(rcs, None);
 
-            c_transfer(&signal_pub, &signal_sec, &*POOL_PARAMS);
+            tx_circuit(signal_pub, signal_sec);
         }
         _ => panic!("Wrong cicruit parameter"),
     };
 
     let bcs = BellmanCS::<Bn256, BuildCS<<Bn256 as Engine>::Fr>>::new(rcs.clone());
 
-    let should_filter_points_at_infinity = false;
+    let should_filter_points_at_infinity = true;
 
     // Import the circuit and create the initial parameters using phase 1
     println!("Creating initial parameters for {}...", circuit_name);
     let params =
         MPCParameters::new(bcs, should_filter_points_at_infinity, radix_directory).unwrap();
+    let params = params.get_params().clone();
+    println!("delta g1 = {:?}", params.vk.delta_g1);
+    println!("delta g2 = {:?}", params.vk.delta_g2);
+    let mut result_params = fawkes_crypto::backend::bellman_groth16::setup::setup::<Bn256, _, _, _>(tx_circuit);
+    result_params.0 = params;
 
     println!("Writing initial parameters to {}.", params_filename);
     let mut f = File::create(params_filename).unwrap();
-    params.write(&mut f).expect("unable to write params");
+    result_params.write(&mut f).expect("unable to write params");
 }
