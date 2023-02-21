439d953d4673f54bbc3e7945faa097adcfe3d19a
Vladimir Popov <rowdyme0@gmail.com>
2023-02-21 15:52:38 +0300

    add dd setup

--- phase2/src/bin/finalize.rs
+++ phase2/src/bin/finalize.rs
@@ -6,16 +6,21 @@ extern crate rand;
 use fawkes_crypto_phase2::parameters::MPCParameters;
 use libzeropool::{
     circuit::tree::{tree_update, CTreePub, CTreeSec},
-    circuit::tx::{c_transfer, CTransferPub, CTransferSec},
-    POOL_PARAMS,
+    circuit::{
+        delegated_deposit::{
+            check_delegated_deposit_batch, CDelegatedDepositBatchPub, CDelegatedDepositBatchSec,
+        },
+        tx::{c_transfer, CTransferPub, CTransferSec},
+    },
     fawkes_crypto::{
+        backend::bellman_groth16::Parameters,
         backend::bellman_groth16::{engines::Bn256, setup::setup},
         circuit::cs::BuildCS,
-        core::signal::Signal,
-        backend::bellman_groth16::Parameters,
         circuit::cs::CS,
-        engines::bn256::Fr
-    }
+        core::signal::Signal,
+        engines::bn256::Fr,
+    },
+    POOL_PARAMS,
 };
 use std::fs::File;
 
@@ -26,11 +31,17 @@ fn tx_circuit<C: CS<Fr = Fr>>(public: CTransferPub<C>, secret: CTransferSec<C>) 
 fn tree_update_circuit<C: CS<Fr = Fr>>(public: CTreePub<C>, secret: CTreeSec<C>) {
     tree_update(&public, &secret, &*POOL_PARAMS);
 }
+fn delegated_deposit<C: CS<Fr = Fr>>(
+    public: CDelegatedDepositBatchPub<C>,
+    secret: CDelegatedDepositBatchSec<C>,
+) {
+    check_delegated_deposit_batch(&public, &secret, &*POOL_PARAMS);
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 4 {
-        println!("Usage: \n<transfer/tree_update> <mpc_params> <out_fawkes_params>");
+        println!("Usage: \n<transfer/tree_update/delegated_deposit> <mpc_params> <out_fawkes_params>");
         std::process::exit(exitcode::USAGE);
     }
     let circuit_name = &args[1];
@@ -55,6 +66,14 @@ fn main() {
 
             tx_circuit(signal_pub, signal_sec);
             setup(tx_circuit)
+        }
+        "delegated_deposit" => {
+            let signal_pub = CDelegatedDepositBatchPub::alloc(rcs, None);
+            signal_pub.inputize();
+            let signal_sec = CDelegatedDepositBatchSec::alloc(rcs, None);
+
+            delegated_deposit(signal_pub, signal_sec);
+            setup(delegated_deposit)
         }
         _ => panic!("Wrong cicruit parameter"),
     };
