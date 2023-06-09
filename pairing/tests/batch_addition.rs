093433963ceff6b7f5920aeb44653ca909f18508
Alexander Filippov <aleksander.fill@gmail.com>
2023-06-09 13:07:41 +0300

    Move batch addition tests

--- pairing/tests/batch_addition.rs
+++ pairing/tests/batch_addition.rs
@@ -1,6 +1,6 @@
 use std::time::Instant;
 
-use pairing::{bn256::{Bn256}, CurveAffine, Engine, CurveProjective, bls12_381::Bls12};
+use fawkes_crypto_pairing_ce::{CurveAffine, bn256::Bn256, Engine, CurveProjective, bls12_381::Bls12};
 use rand::{self, Rand};
 
 fn naive<G: CurveAffine>(a: Vec<G>, b: Vec<G>) -> Vec<G::Projective> {
@@ -22,13 +22,13 @@ fn test_batch_addition_bn256() {
     let b = (0..SAMPLES).map(|_| <Bn256 as Engine>::G1::rand(rng).into_affine()).collect::<Vec<_>>();
     let mut scratch_space = vec![a[0].get_x(); SAMPLES];
 
-    let n = Instant::now();
+    let naive_timer = Instant::now();
     let naive = naive(a.clone(), b.clone());
-    println!("Naive: {}", n.elapsed().as_nanos());
+    println!("Naive: {}", naive_timer.elapsed().as_nanos());
 
-    let ba = Instant::now();
+    let batch_timer = Instant::now();
     CurveAffine::batch_add_assign(&mut a, &b, &mut scratch_space);
-    println!("Batch: {}", ba.elapsed().as_nanos());
+    println!("Batch: {}", batch_timer.elapsed().as_nanos());
     
     let naive: Vec<_> = naive.into_iter().map(|p| p.into_affine()).collect();
     assert!(naive == a);
@@ -43,13 +43,13 @@ fn test_batch_addition_bls12() {
     let b = (0..SAMPLES).map(|_| <Bls12 as Engine>::G1::rand(rng).into_affine()).collect::<Vec<_>>();
     let mut scratch_space = vec![a[0].get_x(); SAMPLES];
 
-    let n = Instant::now();
+    let naive_timer = Instant::now();
     let naive = naive(a.clone(), b.clone());
-    println!("Naive: {}", n.elapsed().as_nanos());
+    println!("Naive: {}", naive_timer.elapsed().as_nanos());
 
-    let ba = Instant::now();
+    let batch_timer = Instant::now();
     CurveAffine::batch_add_assign(&mut a, &b, &mut scratch_space);
-    println!("Batch: {}", ba.elapsed().as_nanos());
+    println!("Batch: {}", batch_timer.elapsed().as_nanos());
     
     let naive: Vec<_> = naive.into_iter().map(|p| p.into_affine()).collect();
     assert!(naive == a);
